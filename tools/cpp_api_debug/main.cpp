cdfc368003a8eaf0f6d4f0e7ac7280ce8f970759
Zhouyang Xie <oceania.xie@gmail.com>
2025-09-05 15:11:47 +1000

    update the debug program

--- tools/cpp_api_debug/main.cpp
+++ tools/cpp_api_debug/main.cpp
@@ -3,15 +3,12 @@
 #include "planner/operator/logical_plan.h"
 
 int main() {
-    auto database = std::make_unique<kuzu::main::Database>("/home/ocea/demo_try_multivertex_wcoj/data/my-sample/kuzu.db");
+    auto database = std::make_unique<kuzu::main::Database>("/home/ocea/demo_try_multivertex_wcoj/data/com-Youtube/kuzu.db");
     auto connection = std::make_unique<kuzu::main::Connection>(database.get());
-    // auto result = connection->query("MATCH (a:Vertex) RETURN *;");
-    std::unique_ptr<kuzu::main::CachedPreparedStatement> left = connection->getClientContext()->getCachedStatement("MATCH (u:Vertex)-[e:Edge]-(v:Vertex) RETURN u.id, v.id;");
-
-    // std::unique_ptr<kuzu::planner::LogicalPlan> left_plan = std::move(left->logicalPlan);
-    std::cout << left->logicalPlan->toString() << std::endl;
-    // kuzu::planner::Schema * left_schema = left_plan->getLastOperator()->getSchema();
-
-    std::unique_ptr<kuzu::main::QueryResult> result = connection->getClientContext()->executeCachedStatement(std::move(left));
+    connection->query("CALL THREADS=1;");
+    connection->query("CALL ENABLE_SEMI_MASK=false;");
+    std::unique_ptr<kuzu::main::CachedPreparedStatement> plan = connection->getClientContext()->getCachedStatement("MATCH (u:Vertex)-[e:Edge]-(v:Vertex) WHERE ((u.id % 8) = 0) AND ((v.id % 8) = 0) HINT ((u JOIN e) JOIN v) RETURN u.id, v.id;");
+    std::cout << plan->logicalPlan->toString() << std::endl;
+    std::unique_ptr<kuzu::main::QueryResult> result = connection->getClientContext()->executeCachedStatement(std::move(plan));
     std::cout << result->toString() << std::endl;
 }
