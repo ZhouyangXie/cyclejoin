65f8ee656c48079db33e9ea441e1ed94d3c2d16c
Zhouyang Xie <oceania.xie@gmail.com>
2025-10-01 18:10:18 +1000

    benchmark the current implementation

--- tools/cpp_api_debug/main.cpp
+++ tools/cpp_api_debug/main.cpp
@@ -1,14 +1,70 @@
 #include <iostream>
 #include "main/kuzu.h"
-#include "planner/operator/logical_plan.h"
+
 
 int main() {
-    auto database = std::make_unique<kuzu::main::Database>("/home/ocea/demo_try_multivertex_wcoj/data/com-Youtube/kuzu.db");
+    auto database = std::make_unique<kuzu::main::Database>("/home/ocea/demo_try_multivertex_wcoj/data/com-Youtube-undirected/kuzu.db");
     auto connection = std::make_unique<kuzu::main::Connection>(database.get());
-    connection->query("CALL THREADS=1;");
-    connection->query("CALL ENABLE_SEMI_MASK=false;");
-    std::unique_ptr<kuzu::main::CachedPreparedStatement> plan = connection->getClientContext()->getCachedStatement("MATCH (u:Vertex)-[e:Edge]-(v:Vertex) WHERE ((u.id % 8) = 0) AND ((v.id % 8) = 0) HINT ((u JOIN e) JOIN v) RETURN u.id, v.id;");
-    std::cout << plan->logicalPlan->toString() << std::endl;
-    std::unique_ptr<kuzu::main::QueryResult> result = connection->getClientContext()->executeCachedStatement(std::move(plan));
-    std::cout << result->toString() << std::endl;
+
+    connection->query("CALL THREADS=16;");
+
+
+    std::string query = "MATCH "
+    "(u0:Vertex)-[u0_u1:Edge]->(u1:Vertex), "
+    "(u0:Vertex)-[u0_v0:Edge]->(v0:Vertex), "
+    "(u0:Vertex)-[u0_v1:Edge]->(v1:Vertex), "
+    "(u1:Vertex)-[u1_v0:Edge]->(v0:Vertex), "
+    "(u1:Vertex)-[u1_v1:Edge]->(v1:Vertex), "
+    "(v0:Vertex)-[v0_v1:Edge]->(v1:Vertex) "
+    "WHERE ((u0.label % 16) = 0) AND ((u1.label % 16) = 1) AND ((v0.label % 16) = 0) AND ((v1.label % 16) = 1) "
+    "RETURN COUNT(*);";
+
+    std::string query_hint_wcoj = "MATCH "
+    "(u0:Vertex)-[u0_u1:Edge]->(u1:Vertex), "
+    "(u0:Vertex)-[u0_v0:Edge]->(v0:Vertex), "
+    "(u0:Vertex)-[u0_v1:Edge]->(v1:Vertex), "
+    "(u1:Vertex)-[u1_v0:Edge]->(v0:Vertex), "
+    "(u1:Vertex)-[u1_v1:Edge]->(v1:Vertex), "
+    "(v0:Vertex)-[v0_v1:Edge]->(v1:Vertex) "
+    "WHERE ((u0.label % 16) = 0) AND ((u1.label % 16) = 1) AND ((v0.label % 16) = 0) AND ((v1.label % 16) = 1) "
+    "HINT ((((((u0 JOIN u0_v1 JOIN v1) MULTI_JOIN u0_v0 MULTI_JOIN v0_v1) JOIN v0) MULTI_JOIN u0_u1 MULTI_JOIN u1_v1) JOIN u1) JOIN u1_v0 )"
+    "RETURN COUNT(*);";
+
+    // kuzu baseline
+    {
+        connection->query("CALL ENABLE_SEMI_MASK=true;");
+        connection->query("CALL ENABLE_MULTIWAY_INTERSECT=false;");
+        auto result = connection->query(query);
+        std::cout << "kuzu baseline: " << result->getQuerySummary()->getExecutionTime() << std::endl;
+    }
+    // kuzu baseline no semi-mask
+    {
+        connection->query("CALL ENABLE_SEMI_MASK=false;");
+        connection->query("CALL ENABLE_MULTIWAY_INTERSECT=false;");
+        auto result = connection->query(query);
+        std::cout << "kuzu baseline no semi-mask(" << result->toString() << "): " << result->getQuerySummary()->getExecutionTime() << std::endl;
+    }
+    // wcoj
+    {
+        connection->query("CALL ENABLE_SEMI_MASK=true;");
+        connection->query("CALL ENABLE_MULTIWAY_INTERSECT=false;");
+        auto result = connection->query(query_hint_wcoj);
+        std::cout << "wcoj(" << result->toString() << "): " <<  result->getQuerySummary()->getExecutionTime() << std::endl;
+    }
+    // wcoj no semi-mask
+    {
+        connection->query("CALL ENABLE_SEMI_MASK=false;");
+        connection->query("CALL ENABLE_MULTIWAY_INTERSECT=false;");
+        auto result = connection->query(query_hint_wcoj);
+        std::cout << "wcoj no semi-mask(" << result->toString() << "): " << result->getQuerySummary()->getExecutionTime() << std::endl;
+    }
+    // multiway (no semi-mask)
+    {
+        connection->query("CALL ENABLE_SEMI_MASK=false;");
+        connection->query("CALL ENABLE_MULTIWAY_INTERSECT=true;");
+        auto result = connection->query(query);
+        std::cout << "multiway(" << result->toString() << "): " << result->getQuerySummary()->getExecutionTime() << std::endl;
+    }
+
+    return 0;
 }
