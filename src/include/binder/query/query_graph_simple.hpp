a0fec8e5a920e01eef78d009784e000f8be28fcf
Zhouyang Xie <oceania.xie@gmail.com>
2026-03-16 16:47:24 +1100

    do not remove the branches, to avoid BinJoin on mid of build side path

--- src/include/binder/query/query_graph_simple.hpp
+++ src/include/binder/query/query_graph_simple.hpp
@@ -269,7 +269,7 @@ Graph pathToGraph(const Path& path) {
 
 void extendPath(Path& path, const Graph& graph, const Nodes& visited) {
     const Node& end = path.back();
-    if (graph.getNeighbors(end).size() > 2) {
+    if (graph.getNeighbors(end).size() != 2) {
         if (visited.contains(end) || end == path[0]) {
             path.clear();
         }
@@ -290,21 +290,6 @@ void extendPath(Path& path, const Graph& graph, const Nodes& visited) {
 
 std::vector<Path> findBasicCycleIntersection(const Graph& graph_) {
     Graph graph = graph_.copy();
-    do {
-        Nodes to_remove;
-        for (const Node& u : graph.nodes) {
-            if (graph.getNeighbors(u).size() < 2) {
-                to_remove.insert(u);
-            }
-        }
-        if (to_remove.size() == 0) {
-            break;
-        }
-        for (const Node& u : to_remove) {
-            graph.removeNode(u);
-        }
-    } while (true);
-
     std::vector<Path> paths;
     Nodes visited;
     for (const Node& start : graph.nodes) {
